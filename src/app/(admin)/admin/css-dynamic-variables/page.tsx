90e56cc30c0f9bc3ff184dcfaad9654c40bbdc73
Phill Anderson <mir18.enktaivan@gmail.com>
2024-05-01 12:41:10 +0800

    24.5.1

--- src/app/(admin)/admin/css-dynamic-variables/page.tsx
+++ src/app/(admin)/admin/css-dynamic-variables/page.tsx
@@ -2,22 +2,29 @@
 import React, { useState } from 'react'
 import './css-dynamic-vars.css'
 import ParticleButton from '@/components/ui/button/particle-button/ParticleButton'
+import { Button } from '@/components/ui/button'
+import { Flex } from '@/components/ui/containers/flex/Flex'
 const CssDynamicVariablesPage = () => {
-    const [color, setColor] = useState('#ff0000')
+    const [color, setColor] = useState('chocolate')
 
     const handleColorChange = () => {
-    setColor('#00ff00')
-}
+        setColor((prev) => (prev === 'chocolate' ? 'green' : 'chocolate'))
+    }
 
     return (
-        <div>
+        <Flex className=" flex-col gap-10">
             <h3>CssDynamicVariablesPage:</h3>
-            <button onClick={handleColorChange}>Change Color</button>
-            <div className="dynamic-vars-example" style={{ '--my-color': color } as React.CSSProperties}>
-                This is my element
+            <Button variant="outline" onClick={handleColorChange}>
+                Өнгийг минь солиод аль
+            </Button>
+            <div className="dynamic-vars-example">
+                <p style={{ '--my-color': color } as React.CSSProperties}>This is my element</p>
+            </div>
+            <div className="dynamic-vars-example">
+                <p>aaa</p>
             </div>
             <ParticleButton />
-        </div>
+        </Flex>
     )
 }
 
