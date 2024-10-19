0968995f9758fbe0899027326f0918f9f6cbc108
Phill-Anderson <eaglesquad86@gmail.com>
2024-10-19 19:31:27 +0800

    work graphic

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,9 +1,15 @@
 import { Flex } from '@/components/ui/containers/flex/Flex'
+import MainPlan from '@/features/work-plan/MainPlan'
 import { NextPage } from 'next'
 import React from 'react'
 
 const AdminHome: NextPage = () => {
-    return <div>Admin dashboard home page ...</div>
+    return (
+        <Flex className="gap-10 flex-col">
+            <h3>Front-end төслийн ажлын график</h3>
+            <MainPlan />
+        </Flex>
+    )
 }
 
 export default AdminHome
